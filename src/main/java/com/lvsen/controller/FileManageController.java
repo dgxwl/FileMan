9ee684a7261712c590ba37b1e283fb85db7e6b03
dgxwl <1029325214@qq.com>
2019-04-22 18:00:58 +0800

    好了

--- src/main/java/com/lvsen/controller/FileManageController.java
+++ src/main/java/com/lvsen/controller/FileManageController.java
@@ -13,14 +13,17 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.lvsen.domain.ResponseResult;
 import com.lvsen.entity.FileVO;
+import com.lvsen.service.FileManageService;
 import com.lvsen.service.IFileManageService;
 import com.lvsen.util.StringUtils;
 import com.lvsen.util.ZipUtils;
@@ -41,7 +44,7 @@ public class FileManageController extends BaseController {
 	 */
 	@RequestMapping(value = "/getFileList/{dept}", method = RequestMethod.POST)
 	public ResponseResult getFileList(@PathVariable String dept, String dir, HttpSession session) {
-		if (StringUtils.isNullOrEmpty(dept)) return new ResponseResult(-100, "目录参数不能为空!");
+		if (StringUtils.multiIsNullOrEmpty(dept, dir)) return new ResponseResult(-100, "目录参数不能为空!");
 		File dirFile = new File(session.getServletContext().getRealPath(dept + dir));
 		//TODO 校验权限
 		
@@ -52,11 +55,149 @@ public class FileManageController extends BaseController {
 			List<FileVO> list = fileManageService.getFileList(dirFile);
 			rr = new ResponseResult();
 			rr.setData(list);
+			rr.setTotal(list.size());
 		} catch (Exception e) {
 			e.printStackTrace();
 			rr = new ResponseResult(-100, "服务器异常: " + e.getMessage());
 		}
 		return rr;
+	}
+	
+	/**
+	 * 创建目录
+	 * @return
+	 */
+	@RequestMapping(value = "/createDir", method = RequestMethod.POST)
+	public ResponseResult createDir(String dir, HttpSession session) {
+		//TODO 校验权限
+		
+		
+		if (StringUtils.isNullOrEmpty(dir)) return new ResponseResult(-100, "目录名不能为空!");
+		String realPath = session.getServletContext().getRealPath(dir);
+		
+		File f = new File(realPath);
+		if (f.exists()) return new ResponseResult(-100, "该文件夹已存在");
+		
+		try {
+			boolean succeed = f.mkdirs();
+			if (succeed) {
+				return new ResponseResult("创建成功");
+			} else {
+				return new ResponseResult(-100, "创建失败");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseResult(-100, "服务器异常: " + e.getMessage());
+		}
+	}
+	
+	/**
+	 * 上传文件
+	 * @param path
+	 * @param file
+	 * @param session
+	 * @return
+	 */
+	@RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+	public ResponseResult uploadFile(String path, MultipartFile file, HttpSession session) {
+		//TODO 校验权限
+		
+		if (StringUtils.isNullOrEmpty(path)) return new ResponseResult(-100, "上传路径不能为空!");
+		if (file == null) return new ResponseResult(-100, "请选择文件进行上传");
+		
+		try {
+			String realPath = session.getServletContext().getRealPath(path);
+			FileUtils.copyInputStreamToFile(file.getInputStream(), new File(realPath, file.getOriginalFilename()));
+			return new ResponseResult("上传成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseResult(-100, "服务器异常: " + e.getMessage());
+		}
+	}
+	
+	/**
+	 * 移动文件/文件夹
+	 * @param oldPath
+	 * @param newPath
+	 * @return
+	 */
+	@RequestMapping(value = "/moveFile", method = RequestMethod.POST)
+	public ResponseResult moveFile(String oldPath, String newPath, HttpSession session) {
+		//TODO 校验权限
+		
+		
+		if (StringUtils.multiIsNullOrEmpty(oldPath, newPath)) return new ResponseResult(-100, "参数不能为空!");
+		
+		String realOldPath = session.getServletContext().getRealPath(oldPath);
+		File old = new File(realOldPath);
+		if (!old.exists()) return new ResponseResult(-100, "源文件/文件夹 " + oldPath + " 不存在");
+		String realNewPath = session.getServletContext().getRealPath(newPath);
+		File neww = new File(realNewPath);
+		if (neww.exists()) return new ResponseResult(-100, "目标文件/文件夹 " + newPath + " 已存在");
+		
+		try {
+			old.renameTo(neww);
+			return new ResponseResult("操作成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseResult(-100, "服务器异常: " + e.getMessage());
+		}
+	}
+	
+	/**
+	 * 重命名文件
+	 * @param oldName
+	 * @param newName
+	 * @return
+	 */
+	@RequestMapping(value = "/renameFile", method = RequestMethod.POST)
+	public ResponseResult renameFile(String oldName, String newName, HttpSession session) {
+		//TODO 校验权限
+		
+		
+		if (StringUtils.multiIsNullOrEmpty(oldName, newName)) return new ResponseResult(-100, "名称不能为空!");
+		if (oldName.equals(newName)) return new ResponseResult(-100, "请输入新名称");
+		
+		String realOldName = session.getServletContext().getRealPath(oldName);
+		File old = new File(realOldName);
+		if (!old.exists()) return new ResponseResult(-100, "源文件/文件夹名 " + oldName + " 不存在");
+		String realNewName = session.getServletContext().getRealPath(newName);
+		File neww = new File(realNewName);
+		if (neww.exists()) return new ResponseResult(-100, "新文件/文件夹名 " + newName + " 已存在");
+		
+		try {
+			old.renameTo(neww);
+			return new ResponseResult("操作成功");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseResult(-100, "服务器异常: " + e.getMessage());
+		}
+	}
+	
+	/**
+	 * 删除文件或文件夹
+	 * @param path
+	 * @return
+	 */
+	@RequestMapping(value = "/delFile", method = RequestMethod.POST)
+	public ResponseResult delDir(String path, HttpSession session) {
+		//TODO 校验权限
+		
+		
+		if (StringUtils.isNullOrEmpty(path)) return new ResponseResult(-100, "路径参数不能为空!");
+		
+		try {
+			String realPath = session.getServletContext().getRealPath(path);
+			File f = new File(realPath);
+			if (!f.exists()) return new ResponseResult(-100, "该文件/文件夹不存在");
+			
+			boolean dir = f.isDirectory();
+			FileManageService.delete(f);
+			return new ResponseResult("已删除该" + (dir? "文件夹" : "文件") + "");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseResult(-100, "服务器异常: " + e.getMessage());
+		}
 	}
 	
 	@RequestMapping(value = "/downDir", method = RequestMethod.POST)
